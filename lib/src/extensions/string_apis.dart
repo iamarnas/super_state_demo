016cd53c898e8e9a095bb26dc7fdd4f5d55b72ba
iamarnas <arnas@Arnass-iMac.alltele.net>
2020-05-31 21:17:23 +0200

    Code correction

--- lib/src/extensions/string_apis.dart
+++ lib/src/extensions/string_apis.dart
@@ -1,9 +1,9 @@
 extension StringValidator on String {
   /// Validate email with [RegExp]. Must match `name@example.co(m)`
   bool get emailValidation {
-    Pattern pattern =
+    var pattern =
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$';
-    RegExp regexp = new RegExp(pattern);
+    var regexp = RegExp(pattern);
     return regexp.hasMatch(this);
   }
 
@@ -18,21 +18,22 @@ extension StringValidator on String {
     // * (?!.*\s) Space not allowed
     //
     // Password must contain A-z, 0-9 and least 6 characters
-    Pattern pattern = r'(?=.*[A-Z])(?=.*[0-9])(?=.{6,})(?!.*[\s])';
-    RegExp regex = new RegExp(pattern);
+    var pattern = r'(?=.*[A-Z])(?=.*[0-9])(?=.{6,})(?!.*[\s])';
+    var regex = RegExp(pattern);
     return regex.hasMatch(this);
   }
 
   /// If [String] value contains space then return `true`
   bool get containsSpace {
-    Pattern pattern = r'(?=.*[\s])';
-    RegExp regexp = new RegExp(pattern);
+    var pattern = r'(?=.*[\s])';
+    var regexp = RegExp(pattern);
     return regexp.hasMatch(this);
   }
 }
 
 extension StringExtensions on String {
   /// [String] starts with a capital letter
-  String get startWithCapitalLetter =>
-      this[0].toUpperCase() + this.substring(1);
+  String get startWithCapitalLetter {
+    return this[0].toUpperCase() + this.substring(1);
+  }
 }
